e010414b70036fd81b3e0d62c816391d209201b0
coolseven <coolseven2013@gmail.com>
2016-10-13 15:03:45 +0800

    add 进度安排草稿.md

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+     * Created for voice2text.
+     * File: index.php
+     * User: cools
+     * Date: 2016/10/13
+     * Time: 0:58
+     * Description :test
+     */
+    echo "let's rock and roll";
+    phpinfo();
